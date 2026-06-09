2197af5bebd8e5f705ce93b0e18c6ab72c5cec15
Mariam Aslam <mariamaslam1210@gmail.com>
2026-06-09 16:45:25 +0500

    Javascript chapter-2

---
+++ javascript-chapter2/app.js
@@ -0,0 +1,14 @@
+// var myname = "Mariam Aslam"
+// var message = "Hello World"
+// alert(myname)
+// alert(message)
+// alert("JOhn Doe")
+// alert("15years old")
+// alert("Certified Mobile Application Development")
+// alert("PIZZA\nPIZZ\nPIZ\nPI\nP")
+var email = "example@example.com"
+alert("My email address is " + email)
+var book = "A smarter way to learn JavaScript"
+alert("I am trying to learn from the Book " + book)
+var icons = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬"
+alert(icons)
