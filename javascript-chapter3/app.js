a294bc5af80f24df8cddfebd7d0a87b0d20f68cb
Mariam Aslam <mariamaslam1210@gmail.com>
2026-06-11 16:50:36 +0500

    JavaScript ( Chapter-3 )

---
+++ javascript-chapter3/app.js
@@ -0,0 +1,10 @@
+// var age = 15 ;
+// alert("I am 15 years old")
+// var visited = 14 ;
+// window.alert("You have visited this site 14 times")
+// var birthyear = 1990
+// document.write("My birth year is 1990\nDatatype of my declared variable is number")
+var Visitorname = "John Doe";
+var Producttitle = "T-shirts";
+var quantity = 5;
+document.write(Visitorname +" ordered " + quantity + " " + Producttitle +" on XYZ Clothhing store ")
