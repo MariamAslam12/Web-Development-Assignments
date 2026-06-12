2dad33f3a87d59c9956d3cbd860e464eda830840
Mariam Aslam <mariamaslam1210@gmail.com>
2026-06-12 18:25:59 +0500

    javascript (Chapter-4)

---
+++ javascript-chapter4/app.js
@@ -0,0 +1,15 @@
+var age =15, name = "Alex" , present = true
+//legal Variable 
+let userName;       
+let _databaseId;    
+let $price;         
+let player1;      
+let total_score;
+//illegal variable 
+// let 1stPlace;       
+// let user-name;      
+// let total amount;  
+// let let;           
+// let email#;
+
+document.write("<h1> Rules for naming JS variables </h1>  Variable names can only contain numbers,letters , legalvariable and $ For example : $my_1stVariable <br> Variables must begin with letter or an underscore For example $name, _name or name <br>  Variable names are case sensitive <br> Variable names should not be JS keywords")
